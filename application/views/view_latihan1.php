3e718f0caacefc05b03dfadb3335a64a58597c62
Mvmid21 <lydoleonardo50@gmail.com>
2021-10-23 06:22:02 +0700

    .

---
+++ application/views/view_latihan1.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk Kita Belajar web programing..!!!<br>
+    NIlai 1 = <?= $nilai1; ?>
+    NIlai 2 = <?= $nilai2; ?>
+    ini hasil dari permodelan dengan methode perjumlahan yaitu 
+    <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
